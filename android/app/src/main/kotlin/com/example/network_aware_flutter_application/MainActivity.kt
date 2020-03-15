e1b47878045dff4422f761f7f7e78061136473cc
msarkrish <rkrishnakumar1997@gmail.com>
2020-03-15 20:04:38 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/network_aware_flutter_application/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.network_aware_flutter_application
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
